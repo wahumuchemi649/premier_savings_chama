eb4d02bd51d124f06d6dd5b0c1f2d722842038a6
wahumuchemi649 <lydiamuchemi260@gmail.com>
2026-06-27 22:00:52 +0300

    endpoint fixes

---
+++ Frontend/config.js
@@ -0,0 +1,4 @@
+
+const BASE_URL = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
+    ? 'http://127.0.0.1:5000'
+    : 'https://premier-savings-chama.onrender.com'
